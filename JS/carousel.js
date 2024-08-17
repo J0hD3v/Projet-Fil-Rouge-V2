051ae29b6a5c8641c58eb7da1688501d7cdc12a2
johan <johanfievet@orange.fr>
2024-08-17 03:29:45 +0200

    premieres modifs du style general

--- JS/carousel.js
+++ JS/carousel.js
@@ -8,10 +8,10 @@ const swiper = new Swiper('.swiper', {
     loop: true,
 
     // If we need pagination
-    pagination: {
+    /* pagination: {
         el: '.swiper-pagination',
         clickable: true,
-    },
+    }, */
 
     // Navigation arrows
     navigation: {
