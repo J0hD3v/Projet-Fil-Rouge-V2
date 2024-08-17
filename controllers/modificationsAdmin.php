312babc18a1dde7fcd15843a6966f50bcc584a77
johan <johanfievet@orange.fr>
2024-08-18 00:17:41 +0200

    maj mineure (redirection sur enregistrement modifs + modif style)

--- controllers/modificationsAdmin.php
+++ controllers/modificationsAdmin.php
@@ -32,42 +32,6 @@ function updateActualites(){
     // Vérifier le format des données
     // pas nécessaire
 
-/*
-        $text_actualite_1 = sanitize($_POST["texte_article_1"]);
-        $text_actualite_2 = sanitize($_POST["texte_article_2"]);
-        $text_actualite_3 = sanitize($_POST["texte_article_3"]);
-        $text_actualite_4 = sanitize($_POST["texte_article_4"]);
-        $text_actualite_5 = sanitize($_POST["texte_article_5"]);
-        $text_actualite_6 = sanitize($_POST["texte_article_6"]);
-        $text_actualite_7 = sanitize($_POST["texte_article_7"]);
-        $text_actualite_8 = sanitize($_POST["texte_article_8"]);
-        $text_actualite_9 = sanitize($_POST["texte_article_9"]);
-        $text_actualite_10 = sanitize($_POST["texte_article_10"]);
-        //
-        $date_actualite_1 = sanitize($_POST["date_article_1"]);
-        $date_actualite_2 = sanitize($_POST["date_article_2"]);
-        $date_actualite_3 = sanitize($_POST["date_article_3"]);
-        $date_actualite_4 = sanitize($_POST["date_article_4"]);
-        $date_actualite_5 = sanitize($_POST["date_article_5"]);
-        $date_actualite_6 = sanitize($_POST["date_article_6"]);
-        $date_actualite_7 = sanitize($_POST["date_article_7"]);
-        $date_actualite_8 = sanitize($_POST["date_article_8"]);
-        $date_actualite_9 = sanitize($_POST["date_article_9"]);
-        $date_actualite_10 = sanitize($_POST["date_article_10"]);
-        //
-        $visibilite_actualite_1 = sanitize($_POST["visibilite_article_1"]);
-        $visibilite_actualite_2 = sanitize($_POST["visibilite_article_2"]);
-        $visibilite_actualite_3 = sanitize($_POST["visibilite_article_3"]);
-        $visibilite_actualite_4 = sanitize($_POST["visibilite_article_4"]);
-        $visibilite_actualite_5 = sanitize($_POST["visibilite_article_5"]);
-        $visibilite_actualite_6 = sanitize($_POST["visibilite_article_6"]);
-        $visibilite_actualite_7 = sanitize($_POST["visibilite_article_7"]);
-        $visibilite_actualite_8 = sanitize($_POST["visibilite_article_8"]);
-        $visibilite_actualite_9 = sanitize($_POST["visibilite_article_9"]);
-        $visibilite_actualite_10 = sanitize($_POST["visibilite_article_10"]);
-*/
-
-
     // Nettoyer les données + Rangement dans un tableau
     $tab_updated_data = [
         'actualite_1' => [1,sanitize($_POST["texte_article_1"]),sanitize($_POST["date_article_1"]),sanitize(isset($_POST["visibilite_article_1"]))],
@@ -91,8 +55,9 @@ function updateActualites(){
         return "Une Erreur est survenue, veuillez réessayer.";
     }
 
-    // Envoi d'un message si fonctionnement correct
-    return "Les données ont bien été enregistrées.";
+    // Redirection à la page d'accueil
+    echo '<script>window.location.href = "/modifications-admin";</script>';
+    return;
 }
 
 
