991c148cd2b66e4d9fccaa1dc6021d87041510d7
johan <johanfievet@orange.fr>
2024-08-19 23:54:03 +0200

    maj style + responsive

--- JS/research-bar.js
+++ JS/research-bar.js
@@ -4,11 +4,16 @@ const espace_contenu = document.getElementById("espace_contenu");
 const tabPictures = espace_contenu.getElementsByClassName("card_media");
 
 
+let message = document.createElement("p");
+message.id = "message";
+message.innerText = "Aucun résultat";
 
 
 // #region Recherche
 research_bar.addEventListener("input", (e) => {
     let recherche = research_bar.value.toUpperCase();   // en upperCase pour faciliter la recherche
+    let recherche_vide = true;
+    let displayed_message = document.querySelector("#message");
 
     for(let picture of tabPictures) {
         
@@ -22,9 +27,16 @@ research_bar.addEventListener("input", (e) => {
             date.includes(recherche) == true
         ) {
             picture.style.display = "flex";
+            recherche_vide = false;
         }
         else {
             picture.style.display = "none";
         }
+    }
+    
+    if(recherche_vide && displayed_message == null) {
+        espace_contenu.append(message);
+    } else if(!recherche_vide && displayed_message != null){
+        displayed_message.remove();
     }
 })
